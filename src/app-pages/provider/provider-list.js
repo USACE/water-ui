8ecd4b8f75934d97d77098f0d3c0cba2acf2ecb3
Adam Scarberry <46494851+adamscarberry@users.noreply.github.com>
2023-04-13 23:05:17 -0400

    new provider list table

--- src/app-pages/provider/provider-list.js
+++ src/app-pages/provider/provider-list.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useConnect } from 'redux-bundler-hook';
-import SimpleTable from '../../app-components/table-simple';
+//import SimpleTable from '../../app-components/table-simple';
 import PageWrapper from '../page-wrapper';
 
 export default function ProviderList() {
@@ -14,9 +14,61 @@ export default function ProviderList() {
     );
   };
 
+  const ProviderTable = ({ providers }) => (
+    <div className="flex flex-col shadow-lg">
+      <div className="overflow-x-auto ">
+        <div className="inline-block min-w-full align-middle">
+          <div className="overflow-hidden shadow ring-1 ring-black ring-opacity-5 md:rounded-lg">
+            <table className="min-w-full divide-y divide-gray-300">
+              <thead className="bg-gray-50">
+                <tr>
+                  <td className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">
+                    Region
+                  </td>
+                  <td className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">
+                    Name
+                  </td>
+                </tr>
+              </thead>
+              <tbody className="divide-y divide-gray-200 bg-white">
+                {providers?.map((p) => (
+                  <tr
+                    key={p.slug}
+                    className={`${
+                      p.type === 'msc' ? 'bg-gray-200 text-gray-800' : null
+                    }`}
+                  >
+                    <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
+                      {p.office_group.toUpperCase()}
+                    </td>
+                    <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
+                      <ProviderLink
+                        title={''.concat(
+                          p.name,
+                          ' (',
+                          p.slug.toUpperCase(),
+                          ')'
+                        )}
+                        href={''.concat(
+                          '/overview/',
+                          `${p.slug.toLowerCase()}`
+                        )}
+                      />
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+
   return (
-    <PageWrapper title="Data Providers" subTitle="SubTitle">
-      <SimpleTable
+    <PageWrapper title="Data Providers" subTitle="">
+      <ProviderTable providers={providers} />
+      {/* <SimpleTable
         headers={['Reports to', 'Name']}
         items={providers}
         itemFields={[
@@ -45,7 +97,7 @@ export default function ProviderList() {
             },
           },
         ]}
-      />
+      /> */}
     </PageWrapper>
   );
 }
