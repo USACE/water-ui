d71948ec7bb710147f9b164cde4542638c85139e
Adam Scarberry <46494851+adamscarberry@users.noreply.github.com>
2023-05-01 12:25:31 -0400

    allow top of con location level

--- src/app-components/charts/dam-profile-chart/chart.js
+++ src/app-components/charts/dam-profile-chart/chart.js
@@ -20,6 +20,7 @@ export default function ReactDamProfileChart() {
 
     const allowedLevels = [
       'Streambed',
+      'Top of Conservation',
       'Top of Dam',
       'Top of Surcharge',
       'Top of Flood',
