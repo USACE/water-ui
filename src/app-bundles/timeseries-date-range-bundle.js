a37e003aeb37d2ef1bec170e2e865d6b78609968
Adam Scarberry <46494851+adamscarberry@users.noreply.github.com>
2023-07-07 11:59:06 -0400

    ts end date default to now unless specified in env

--- src/app-bundles/timeseries-date-range-bundle.js
+++ src/app-bundles/timeseries-date-range-bundle.js
@@ -1,13 +1,21 @@
 import { createSelector } from 'redux-bundler';
 import { subDays } from 'date-fns';
 
+// use env var if defined for testing, otherwise use current datetime
+const defaultEndDate =
+  (import.meta.env.VITE_APP_TS_DEFAULT_END_DATE &&
+    new Date(import.meta.env.VITE_APP_TS_DEFAULT_END_DATE)) ||
+  new Date();
+
+console.log(defaultEndDate);
+
 const timeseriesDateRangeBundle = {
   name: 'timeseriesDateRange',
 
   getReducer: () => {
     const initialData = {
-      beginDate: subDays(new Date('2023-05-05T03:24:00'), 7),
-      endDate: new Date('2023-05-05T03:24:00'),
+      beginDate: subDays(defaultEndDate, 7),
+      endDate: defaultEndDate,
     };
 
     return (state = initialData, { type, payload }) => {
