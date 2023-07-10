0de21d16c1fd5d81ecda18930bc247de60afcc94
Adam Scarberry <46494851+adamscarberry@users.noreply.github.com>
2023-07-10 11:30:41 -0400

    move home card images from remote to local

--- src/app-pages/home.jsx
+++ src/app-pages/home.jsx
@@ -5,6 +5,9 @@ import HomeDam1 from '../images/home/lrh-sutton-dam.jpg';
 import HomeDam2 from '../images/home/nww-lucky-peak-dam.jpg';
 import HomeDam3 from '../images/home/nww-ice-harbor-lock-dam.jpg';
 import LocationCombobox from '../app-components/inputs/location-search-input';
+import LocationCardSrc from '../images/cards/freibergsee-unsplash.jpg';
+import MapCardSrc from '../images/cards/map-unsplash.jpg';
+import DataResourcesCardSrc from '../images/cards/data-screen-unsplash.jpg';
 
 const HomeBgImgArray = [
   { src: HomeDam1 },
@@ -22,48 +25,48 @@ export default function Home() {
     location ? true : false
   );
 
-  const Card1ImgSrc =
-    'https://images.unsplash.com/photo-1516132006923-6cf348e5dee2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8bGFrZXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=700&q=60';
-  const Card2ImgSrc =
-    'https://images.unsplash.com/photo-1551288049-bebda4e38f71?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3270&q=80';
-  // const Card3ImgSrc =
-  //   'https://images.unsplash.com/photo-1543286386-713bdd548da4?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NTh8fGRhdGF8ZW58MHx8MHx8&auto=format&fit=crop&w=700&q=60';
-  const MapCardSrc =
-    'https://images.unsplash.com/photo-1451988336904-b1a6e8846746?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=700&q=60';
+  // const Card1ImgSrc =
+  //   'https://images.unsplash.com/photo-1516132006923-6cf348e5dee2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8bGFrZXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=700&q=60';
+  // const Card2ImgSrc =
+  //   'https://images.unsplash.com/photo-1551288049-bebda4e38f71?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3270&q=80';
+  // // const Card3ImgSrc =
+  // //   'https://images.unsplash.com/photo-1543286386-713bdd548da4?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NTh8fGRhdGF8ZW58MHx8MHx8&auto=format&fit=crop&w=700&q=60';
+  // const MapCardSrc =
+  //   'https://images.unsplash.com/photo-1451988336904-b1a6e8846746?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=700&q=60';
   return (
     <>
       <PageTitle useDefault={true} />
 
-      <div className="pt-0">
+      <div className='pt-0'>
         <div
-          className="flex h-72 max-h-full bg-gray-500 bg-cover bg-center bg-no-repeat px-4 py-16 sm:px-6 sm:pb-24 sm:pt-20 lg:h-128 lg:px-8 lg:pt-8"
+          className='flex h-72 max-h-full bg-gray-500 bg-cover bg-center bg-no-repeat px-4 py-16 sm:px-6 sm:pb-24 sm:pt-20 lg:h-128 lg:px-8 lg:pt-8'
           style={{ backgroundImage: `url(${HomeBgImg.src})` }}
         >
           {/* NOTE: relative and z index styles below are needed to keep the 
           search results above other content which contains opacity adjustments */}
-          <div className="relative z-10 m-auto w-full rounded-md border-2 border-gray-400 bg-gray-300 p-4 sm:w-4/5 md:w-3/5 lg:w-2/5">
-            <div className="relative shadow-lg">
+          <div className='relative z-10 m-auto w-full rounded-md border-2 border-gray-400 bg-gray-300 p-4 sm:w-4/5 md:w-3/5 lg:w-2/5'>
+            <div className='relative shadow-lg'>
               <LocationCombobox
-                label=""
+                label=''
                 value={location}
                 setValue={setLocation}
                 isValid={!location || locationIsValid}
                 setIsValid={setLocationIsValid}
                 isRequired={false}
-                placeholder="Search"
-                className="border-2 border-gray-300"
+                placeholder='Search'
+                className='border-2 border-gray-300'
               />
             </div>
           </div>
         </div>
       </div>
 
-      <div className="-mt-10 bg-blue-900 py-0 text-center text-gray-300 opacity-90">
-        <div className="mx-auto max-w-2xl px-4 py-16 pb-16 sm:px-6 sm:pt-20 sm:text-center lg:max-w-screen-2xl lg:px-8">
-          <h2 className="text-xl font-medium tracking-tight text-white md:text-3xl">
+      <div className='-mt-10 bg-blue-900 py-0 text-center text-gray-300 opacity-90'>
+        <div className='mx-auto max-w-2xl px-4 py-16 pb-16 sm:px-6 sm:pt-20 sm:text-center lg:max-w-screen-2xl lg:px-8'>
+          <h2 className='text-xl font-medium tracking-tight text-white md:text-3xl'>
             U.S. Army Corps of Engineers - Water Management
           </h2>
-          <p className="mt-2 font-normal text-gray-400 md:text-lg">
+          <p className='mt-2 font-normal text-gray-400 md:text-lg'>
             The United States Army Corps of Engineers (USACE) is responsible for
             operating and maintaining more than 700 lock and dam projects
             nationwide. The Access to Water Resources Data - Corps Water
@@ -76,38 +79,38 @@ export default function Home() {
         </div>
       </div>
 
-      <div className="py-8 opacity-90">
-        <div className="mx-auto w-full max-w-7xl px-4 sm:px-6 md:flex md:gap-8 lg:px-8">
-          <a href="/overview">
+      <div className='py-8 opacity-90'>
+        <div className='mx-auto w-full max-w-7xl px-4 sm:px-6 md:flex md:gap-8 lg:px-8'>
+          <a href='/overview'>
             <ImageCard
-              title="Locations"
-              text="View projects and locations by USACE office with overviews by watershed."
-              imgSrc={Card1ImgSrc}
+              title='Locations'
+              text='View projects and locations by USACE office with overviews by watershed.'
+              imgSrc={LocationCardSrc}
             ></ImageCard>
           </a>
-          <a href="/map">
+          <a href='/map'>
             <ImageCard
-              title="Map View"
-              text="Explore USACE water resource locations using an interactive map."
+              title='Map View'
+              text='Explore USACE water resource locations using an interactive map.'
               imgSrc={MapCardSrc}
             ></ImageCard>
           </a>
-          <a href="/">
+          <a href='/'>
             <ImageCard
-              title="Data Resources"
-              text=""
-              imgSrc={Card2ImgSrc}
+              title='Data Resources'
+              text=''
+              imgSrc={DataResourcesCardSrc}
             ></ImageCard>
           </a>
         </div>
       </div>
 
-      <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
-        <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:pb-24 sm:pt-20 sm:text-center lg:max-w-7xl lg:px-8 lg:pt-24">
-          <h2 className="text-3xl font-medium tracking-tight text-gray-900">
+      <div className='mx-auto max-w-7xl px-4 sm:px-6 lg:px-8'>
+        <div className='mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:pb-24 sm:pt-20 sm:text-center lg:max-w-7xl lg:px-8 lg:pt-24'>
+          <h2 className='text-3xl font-medium tracking-tight text-gray-900'>
             Find Water Resources Data across the U.S
           </h2>
-          <p className="mt-2 text-lg text-gray-600">
+          <p className='mt-2 text-lg text-gray-600'>
             Access water resources data such as elevation, precipitation,
             storage and flow status of more than 700 USACE reservior and lock &
             dam projects.
