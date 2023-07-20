d85d330b66deead49331f48c7e92d4ff99d3b929
Adam Scarberry <46494851+adamscarberry@users.noreply.github.com>
2023-07-20 16:40:50 -0400

    optimizations

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import eslint from 'vite-plugin-eslint';
+import { splitVendorChunkPlugin } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -11,6 +12,7 @@ export default defineConfig({
       include: ['./src/**/*.js', './src/**/*.jsx'],
       exclude: [],
     }),
+    [splitVendorChunkPlugin()],
   ],
   build: {
     // set this to 500 after optimizations
